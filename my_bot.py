5357a6246ac0e72aa11e123f70a5b0bcd58ab409
starry-skies <mmazin2028@kehillahstudent.org>
2025-02-04 16:57:31 -0800

    working

--- my_bot.py
+++ my_bot.py
@@ -27,28 +27,31 @@ def should_i_respond(user_message, user_name):
        return False
      
 def respond(user_message, user_name):
- if "robot" == user_message:
+ #works
+ if "up" == user_message:
    return user_message.upper()
+  #works
  if "down" == user_message:
    return user_message.lower()
+  #works
  if "animal" == user_message or "pet" == user_message:
    animal_jokes = ["A polar bear’s hair is not white – it’s colourless!", "There are no male or female earthworms. All earthworms have both male and female parts – but it still takes two of them to reproduce.", "Huskies can run at speeds of around 31km per hour (20mph)!", "An eagle’s eyes are at least four times sharper than a human’s!", "Giant tortoise from the Galápagos Islands near Ecuador can weigh up to 250kg (550lbs) – the same as a brown bear!", "The lion has the loudest roar of all the big cats. It can be heard as far as 5km (3 miles) away.","Humans share 98.8 percent of chimpanzee DNA. But even with DNA so similar, humans and chimps have around 35 million differences between them."]
    random_animal_jokes = random.choice(animal_jokes)
    return(f"Here's a fun fact about animals! " + random_animal_jokes)
- 
+  #works
  if "corny" == user_message or "cheesy" == user_message:
    corny_jokes = [ "Why do crabs never give to charity? Because they’re shellfish.", "For a fungi to grow, you must give it as mushroom as possible.", "I was going to grow some herbs, but I couldn’t find the thyme.", "What do you call a sheep who can sing and dance? Lady Ba Ba.", "What do you call a French man wearing sandals? Philipe Fallop.", "Who won the neck decorating contest? It was a tie.", "What do you call the security guards for Samsung? Guardians of the galaxy."]
    random_jokes = random.choice(corny_jokes)
    return(random_jokes)
- 
- if "mixed" == user_message:
-     user_message = user_message.replace("a", "A")
-     user_message = user_message.replace("e", "E")
-     user_message = user_message.replace("i", "I")
-     user_message = user_message.replace("o", "O")
-     user_message = user_message.replace("u", "U")
-     return user_message
-
+  #doesn't work
+ if user_message == "mixed":
+  user_message = user_message.replace("a", "A")
+  user_message = user_message.replace("e", "E")
+  user_message = user_message.replace("i", "I")
+  user_message = user_message.replace("o", "O")
+  user_message = user_message.replace("u", "U")
+  return user_message
+  #doesn't work
  if "hungry" == user_message:
    return ("Do you like Italian, Mexican, Japanese, or Indian food?")
  if "Italian" in user_message:
@@ -67,29 +70,29 @@ def respond(user_message, user_name):
      indian = ["biryani", "samosas", "tikka masala", "pani puri", "aloo gobi", "masala dosa", "chana masala"]
      indian_food = random.choice(indian)
      return (f"I suggest you try" + indian_food + "!")
- 
+ #works
  if "cold" == user_message:
     return ("Make sure to wear a jacket!")
+#doesn't work
  if "random" == user_message:
-     key = float(input("Pick a number!"))
+     key = 4
      new_text = ""
      for letter in user_message:
           old_position = alphabet.find(letter)
           if old_position == -1:
             new_text += " "
           else:
-            new_position = old_position + key
-            new_position = new_position % len(alphabet)
+            new_position = (old_position + key) % len(alphabet)
             new_letter = alphabet[new_position]
             new_text += new_letter
      return new_text
+   #doesn't work
  if "time" == user_message:
-      def countdown():
-        for i in range(10):
-          countdown -= 1
-          return countdown
-        if countdown == 0:
-            return "Blast off!!!"
-      countdown()
+    for i in range(10, 0):
+     return i
+    if i == 0:
+     return "Blast off!!!"
+
+    #doesn't work
  if "backward" == user_message:
    return True
