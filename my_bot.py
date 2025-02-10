093f9a193007dc929eed89c1ec284ec3288b0911
starry-skies <mmazin2028@kehillahstudent.org>
2025-02-10 08:39:09 -0800

    working

--- my_bot.py
+++ my_bot.py
@@ -1,9 +1,9 @@
 
 import random
 import string
-global state
-state = "start"
 alphabet = string.ascii_lowercase
+global state
+
 def should_i_respond(user_message, user_name):
      if "up" in user_message:
       return True
@@ -37,28 +37,41 @@ def should_i_respond(user_message, user_name):
        return False
      
 def my_respond(user_message, user_name):
-  if "up" == user_message:
-   return user_message.upper()
-  if "down" == user_message:
+  global state
+  state = "start"
+  if "up" in user_message:
+   state = "up"
+   return "What word should I capitilize?"
+  if state == "up":
+    message = user_message.upper()
+    state = "start"
+    return message
+  if "down" in user_message:
+    state = "down"
+    return "What word should I lowercase?"
+  if state == "down":
     return user_message.lower()
-  if "animal" == user_message or "pet" == user_message:
+  if "animal" in user_message or "pet" == user_message:
    animal_jokes = ["A polar bear’s hair is not white – it’s colourless!", "There are no male or female earthworms. All earthworms have both male and female parts – but it still takes two of them to reproduce.", "Huskies can run at speeds of around 31km per hour (20mph)!", "An eagle’s eyes are at least four times sharper than a human’s!", "Giant tortoise from the Galápagos Islands near Ecuador can weigh up to 250kg (550lbs) – the same as a brown bear!", "The lion has the loudest roar of all the big cats. It can be heard as far as 5km (3 miles) away.","Humans share 98.8 percent of chimpanzee DNA. But even with DNA so similar, humans and chimps have around 35 million differences between them."]
    random_animal_jokes = random.choice(animal_jokes)
    return(f"Here's a fun fact about animals! " + random_animal_jokes)
-  if "joke" == user_message:
+  if "joke" in user_message:
    corny_jokes = [ "Why do crabs never give to charity? Because they’re shellfish.", "For a fungi to grow, you must give it as mushroom as possible.", "I was going to grow some herbs, but I couldn’t find the thyme.", "What do you call a sheep who can sing and dance? Lady Ba Ba.", "What do you call a French man wearing sandals? Philipe Fallop.", "Who won the neck decorating contest? It was a tie.", "What do you call the security guards for Samsung? Guardians of the galaxy."]
    random_jokes = random.choice(corny_jokes)
    return(random_jokes)
-  if user_message == "capitilize":
+  if  "capitilize" in user_message:
    user_message = user_message.replace("a", "A")
    user_message = user_message.replace("e", "E")
    user_message = user_message.replace("i", "I")
    user_message = user_message.replace("o", "O")
    user_message = user_message.replace("u", "U")
    return user_message
-  if "cold" == user_message:
+  if "cold" in user_message:
     return "Make sure to wear a jacket!"
-  if "encrypt" == user_message:
+  if "encrypt" in user_message:
+    state = "encrypt"
+    return "What word should I encrypt?"
+  if state == "encrypt":
     key_choices = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
     random_key = random.choice(key_choices)
     new_text = ""
@@ -71,35 +84,42 @@ def my_respond(user_message, user_name):
             new_letter = alphabet[new_position]
             new_text += new_letter
     return new_text
-  if user_message == "time":
+  if "time" in user_message:
     random_time = random.randint(1, 100)
     countdown_list = []
     for i in range(random_time, 0, -1):
      i += random_time
-     countdown_list.append(i)
-    countdown_list.append("Blast off!!!")
-    return countdown_list
-  if "backward" == user_message:
+    return i
+  return countdown_list("Blast off!!!")
+  if "backward" in user_message:
     state = "backwards"
     return "What word should I spell backwards?"
   if state == "backwards":
     reverse = user_message[::-1]
     return reverse
-  if "hungry" == user_message:
+  if "hungry" in user_message:
     return "Do you want to have Italian, Mexican, Japanese, or Indian food?"
-  if "Italian" or "italian" == user_message:
+  if "Italian" or "italian" in user_message:
+    state == "Italian"
+  if state == "Italian":
     italian = ["lasagna", "risotto", "cacio e pepe", "tiramisu", "arancini", "focaccia", "gelato"]
     italian_food = random.choice(italian)
     return (f"I suggest you try " + italian_food + "!")
   if "Mexican" or "mexican" in user_message:
+    state == "Mexican"
+  if state == "Mexican":
      mexican = ["tamales", "enchiladas", "pozole", "churros", "birria", "fajitas", "tortas"]
      mexican_food = random.choice(mexican)
      return (f"I suggest you try " + mexican_food + "!")
-  if "Japanese" or "japanese"in user_message:
+  if "Japanese" or "japanese" in user_message:
+      state == "Japanese"
+  if state == "Japanese":
      japanese = ["ramen", "takoyaki", "gyudon", "sushi", "tempura", "udon", "shabu-shabu"]
      japanese_food = random.choice(japanese)
      return(f"I suggest you try " + japanese_food + "!")
   if "Indian" or "indian" in user_message:
+      state == "Indian"
+  if state == "Indian":
      indian = ["biryani", "samosas", "tikka masala", "pani puri", "aloo gobi", "masala dosa", "chana masala"]
      indian_food = random.choice(indian)
      return f"I suggest you try " + indian_food + "!"
